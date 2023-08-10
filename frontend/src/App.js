0fbc6863aa1e42aa565298c9cae5954872f63151
shobhitcs <shobhitchauhancs@gmail.com>
2023-08-10 18:36:42 +0530

    added snackbar message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './components/Home';
@@ -8,7 +9,7 @@ import { useSelector } from 'react-redux';
 import DashBoard from './components/Dashboard';
 import { useVerifyUser } from './hooks/useVerifyUser';
 import { useEffect } from 'react';
-import { CircularProgress } from '@mui/material';
+import { CircularProgress,Snackbar,Alert } from '@mui/material';
 
 function App() {
   const user = useSelector((state) => {
@@ -18,30 +19,43 @@ function App() {
   //   return state.persons.person;
   // })
 
-  const { verifystate , isVerifying } = useVerifyUser();
+  const { verifystate, isVerifying } = useVerifyUser();
 
   useEffect(() => {
     verifystate();
-  },[]);
+  }, []);
+  const [open, setOpen] = React.useState(true);
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
 
 
   return (
     <div className="App">
       <BrowserRouter>
-        
+
         <Navbar />
         {isVerifying && (
-          <div className="load-verify" style={{paddingTop: '70px', minHeight:'100vh', display: 'flex', alignItems: 'center',justifyContent: 'center' }}>
+          <div className="load-verify" style={{ paddingTop: '70px', minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <CircularProgress />
           </div>
         )}
         {!isVerifying &&
-        <Routes>
-          <Route path='/' element={!user ? <Home /> : <DashBoard />} />
-          <Route path='/login' element={!user ? <Login /> : <Navigate to="/" />} />
-          <Route path='/signup' element={!user ? <Signup /> : <Navigate to="/" />} />
-        </Routes>}
+          <Routes>
+            <Route path='/' element={!user ? <Home /> : <DashBoard />} />
+            <Route path='/login' element={!user ? <Login /> : <Navigate to="/" />} />
+            <Route path='/signup' element={!user ? <Signup /> : <Navigate to="/" />} />
+          </Routes>}
       </BrowserRouter>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="info" sx={{ width: '100%' }}>
+        Please be aware that our website's backend is hosted on a free server, which might lead to initial response times of over 30 seconds; subsequent responses will be much quicker as the server goes to sleep after periods of inactivity.
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
